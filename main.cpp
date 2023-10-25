4292b95737faf64b30b038f774277890ad35dd37
UbuntuFilip <ubuntu@budgie.com>
2023-10-25 20:44:27 +0200

    Colorimetric (perceptual luminance-preserving) conversion to grayscale

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <fstream>
+#include <math.h>
 
 char grey[10] = {'@', '%', '#', '*', '+', '=', '-', ':', '.', ' '};
 
+
+double gammaExpr(int c){
+	double cl = c / 255.0;
+
+	if(cl <= 0.04045) return cl/12.92;
+	
+	return pow((cl+0.055)/1.055,2.4);
+}
 
 int main(int argc, char** argv){
 
@@ -14,17 +23,32 @@ int main(int argc, char** argv){
 
 	in >> word >> w >> h >> word;
 
-	int value;
+	int r, g, b;
+	
+	//std::ofstream out("output.ppm", std::ios_base::out | std::ios_base::binary);
+
+	//out << "p3\n" << w << " " << h << "\n" << "255\n";
 
 	system("clear");
 
 	for(int y = 0; y < h; y++){
 		for(int x = 0; x < w; x++){
-			in >> value >> value >> value;
-			std::cout << " " << grey[int(value/25.5)];
+			in >> r >> g >> b;
+			
+			double gama = gammaExpr(r)*0.2126+gammaExpr(g)*0.7152+gammaExpr(b)*0.0722;
+
+			std::cout << grey[int(gama * 10)] << " ";
+
+			//out << int(gama * 255) <<"\n"<< int(gama * 255) <<"\n"<< int(gama * 255) <<"\n";
+			//out << r << "\n" << g << "\n" << b << "\n";
+
 		}
 		std::cout << "\n";
 	}
+	
+	std::cout << pow(2.0, 10.0);
+	
+	//out.close();
 	in.close();
 	return 0;
 }
