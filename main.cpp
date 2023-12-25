3e1f7f07f12de9eb8d08d7acfb1c2204570ed8fb
FilipUbuntu <filip@ubuntu.com>
2023-12-25 16:44:52 +0100

    bench

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <math.h>
+#include <chrono>
 
 char grey[10] = {'@', '%', '#', '*', '+', '=', '-', ':', '.', ' '};
 
@@ -15,8 +16,9 @@ double gammaExpr(int c){
 
 int main(int argc, char** argv){
 
-	std::string filePath = "test.ppm";
+	std::string filePath = "123.ppm";
 	std::ifstream in(filePath);
+	
 
 	std::string word;
 	int w, h;
@@ -24,31 +26,49 @@ int main(int argc, char** argv){
 	in >> word >> w >> h >> word;
 
 	int r, g, b;
-	
-	//std::ofstream out("output.ppm", std::ios_base::out | std::ios_base::binary);
 
-	//out << "p3\n" << w << " " << h << "\n" << "255\n";
+	//system("clear");
 
-	system("clear");
+	for(int i = 0; i < 10; i++){
 
-	for(int y = 0; y < h; y++){
-		for(int x = 0; x < w; x++){
-			in >> r >> g >> b;
-			
-			double gama = gammaExpr(r)*0.2126+gammaExpr(g)*0.7152+gammaExpr(b)*0.0722;
+		std::string outName = "output0.txt";
+		outName[6] = char(i + 48);
 
-			std::cout << grey[int(gama * 10)] << " ";
+		auto start = std::chrono::high_resolution_clock::now();
 
-			//out << int(gama * 255) <<"\n"<< int(gama * 255) <<"\n"<< int(gama * 255) <<"\n";
-			//out << r << "\n" << g << "\n" << b << "\n";
+		std::ofstream out(outName);
 
+		for(int y = 0; y < h; y++){
+			for(int x = 0; x < w; x++){
+				in >> r >> g >> b;
+
+				double gama = gammaExpr(r)*0.2126+gammaExpr(g)*0.7152+gammaExpr(b)*0.0722;
+
+				//std::cout << grey[int(gama * 10)] << " ";
+				out << grey[int(gama * 10)] << " ";
+
+
+
+				//out << int(gama * 255) <<"\n"<< int(gama * 255) <<"\n"<< int(gama * 255) <<"\n";
+				//out << r << "\n" << g << "\n" << b << "\n";
+
+			}
+			//std::cout << "\n";
+			out << "\n";
 		}
-		std::cout << "\n";
+
+		out.close();
+
+		auto stop = std::chrono::high_resolution_clock::now();
+		auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+
+		std::cout << i << " done in " << duration.count() / 1000000.0 << "\n";
 	}
+
+	//std::cout << pow(2.0, 10.0);
+	//out << pow(2.0, 10.0);
 	
-	std::cout << pow(2.0, 10.0);
 	
-	//out.close();
 	in.close();
 	return 0;
 }
